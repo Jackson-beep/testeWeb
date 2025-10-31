6ec75f93de6a01def46065fdf55bbb3e95009919
Mateus Antunes Ferreira <mateusantunesferreira10@gmail.com>
2025-10-30 21:39:58 -0300

    Add files via upload

---
+++ aula-30-10-2025/meuscript.js
@@ -0,0 +1,14 @@
+function ligar() {
+    document.getElementById("lamp").src = "img/pic_bulbon.gif";
+}
+function desligar() {
+    document.getElementById("lamp").src = "img/pic_bulboff.gif";
+}
+function trocarImagem() {
+    var lampada = document.getElementById("lamp2");
+    if (document.getElementById("lamp2").src = "img/51NoADt9NmL.jpg";) {
+         document.getElementById("lamp2").src = "img/homer-simpson-gif-1547466477.avif";
+    } else {
+       document.getElementById("lamp2").src = "img/51NoADt9NmL.jpg";
+    }
+}
